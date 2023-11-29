ad19321b65fc787028ec2e36cb18df3bb145b73e
Dmitry Gorokhov <gorokhov86@gmail.com>
2023-11-29 19:37:18 +0300

    fix welcome template

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -24,7 +24,7 @@ new class extends Component
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}" wire:navigate>
+                    <a href="/" wire:navigate>
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
                     </a>
                 </div>
@@ -42,15 +42,12 @@ new class extends Component
                         {{ __('My ToDo') }}
                     </x-nav-link>
                 </div>
-                @endauth
-
-
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('todoindex')" :active="request()->routeIs('todoindex')" wire:navigate>
                         {{ __('Public ToDoIndex') }}
                     </x-nav-link>
                 </div>
-
+                @endauth
             </div>
 
             <!-- Settings Dropdown -->
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="font-semibold text-3xl text-gray-800 leading-tight">
             {{ __('Welcome to ToDo lists service!!!') }}
         </h2>
     </x-slot>
-    
 </x-app-layout>
